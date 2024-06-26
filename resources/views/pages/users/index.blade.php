a86b7bbfdea48075de69aad8900bc287f9f55813
Nath <ananthapindika739@gmail.com>
2024-06-26 15:53:48 +0700

    style: Improve grammar in user and category section

--- resources/views/pages/users/index.blade.php
+++ resources/views/pages/users/index.blade.php
@@ -37,7 +37,7 @@
                     <div class="col-12">
                         <div class="card">
                             <div class="card-header">
-                                <h4>All Posts</h4>
+                                <h4>All Users</h4>
                             </div>
                             <div class="card-body">
 
