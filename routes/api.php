35f9c040591048427ae7a3b480c7c019554ea992
Nath <ananthapindika739@gmail.com>
2024-07-01 00:49:56 +0700

    feat : add Category and Product Controller API

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\CategoryController;
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -12,3 +15,8 @@ Route::get('/user', function (Request $request) {
 Route::post('/login', [AuthController::class, 'login']);
 //logout
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+//products
+Route::apiResource('/api-products', ProductController::class)->middleware('auth:sanctum');
+//categories
+Route::apiResource('/api-categories', CategoryController::class)->middleware('auth:sanctum');
